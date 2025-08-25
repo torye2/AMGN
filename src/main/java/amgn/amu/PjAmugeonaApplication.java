e2d98419c839f265a2b019c59adf83ac0cadb9cc
torye2 <rkdgywh6343@gmail.com>
2025-08-25 11:45:39 +0900

    Base

---
+++ src/main/java/amgn/amu/PjAmugeonaApplication.java
@@ -0,0 +1,13 @@
+package amgn.amu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjAmugeonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjAmugeonaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/amgn/amu/PjAmugeonaApplicationTests.java
@@ -0,0 +1,13 @@
+package amgn.amu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjAmugeonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
