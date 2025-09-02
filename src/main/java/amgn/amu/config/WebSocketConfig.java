2542d95a2c14dcce771d4c9b2044a22982fb295b
sanghyeok07 <3400jkh@naver.com>
2025-09-02 10:32:56 +0900

    채팅

---
+++ src/main/java/amgn/amu/config/WebSocketConfig.java
@@ -0,0 +1,62 @@
+package amgn.amu.config;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS()
+                .setInterceptors(httpSessionHandshakeInterceptor());
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public HandshakeInterceptor httpSessionHandshakeInterceptor() {
+        return new HandshakeInterceptor() {
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest request,
+                                           ServerHttpResponse response,
+                                           WebSocketHandler wsHandler,
+                                           Map<String, Object> attributes) throws Exception {
+
+                if (request instanceof ServletServerHttpRequest servletRequest) {
+                    HttpSession session = servletRequest.getServletRequest().getSession(false);
+                    if (session != null) {
+                        attributes.put("loginUser", session.getAttribute("loginUser"));
+                    }
+                }
+                return true;
+            }
+
+            @Override
+            public void afterHandshake(ServerHttpRequest request,
+                                       ServerHttpResponse response,
+                                       WebSocketHandler wsHandler,
+                                       Exception ex) {
+            }
+        };
+    }
+}
